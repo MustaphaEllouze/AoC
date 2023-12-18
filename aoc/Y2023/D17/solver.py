31742b3046f746fa36b35c5c1d19199d5f12d16d
MustaphaEllouze <ellouze.mostafa@gmail.com>
2023-12-18 07:35:02 +0100

    MDR TUEZ MOI

--- aoc/Y2023/D17/solver.py
+++ aoc/Y2023/D17/solver.py
@@ -3,53 +3,57 @@ from aoc.tools import ABCSolver, Map
 from bisect import insort
 from collections import defaultdict
 
+REVERSED = dict(zip('SDURL', 'SUDLR'))
+
 class Solver(ABCSolver):
 
     def solve(self, part2: bool = False) -> tuple[Any, str]:
 
         map = Map([[int(e) for e in list(line)] for line in self.data])
 
-        # Case, heatloss, nombres cases même direction, même direction, path, subpath
-        current = ((0,0),0,0, 'S','S')
-        visited = defaultdict(list)
-        to_visit = [current]
+        if not part2 : 
 
-        best_path = None
+            # Case, heatloss, nombres cases même direction, même direction, path, subpath
+            current = ((0,0),0,0, 'S','S')
+            visited = defaultdict(list)
+            to_visit = [current]
 
-        while to_visit:
-            coords, heatloss, nb_direction, direction, path = to_visit.pop(0)
-            if path[-3:] in visited[coords] : continue
-            visited[coords].append(path[-3:])
-            print(coords, len(to_visit))
-            if coords == (map.height-1, map.width-1) : 
-                best_path = heatloss
-                print(path)
-                break
-            for nei,dir_nei in [
-                (map.up(    *coords),'U'), 
-                (map.down(  *coords),'D'), 
-                (map.right( *coords),'R'), 
-                (map.left(  *coords),'L'),
-            ]:
-                if nei and (dir_nei!=direction or nb_direction<3)\
-                and not((dir_nei in 'DU' and direction in 'DU' and dir_nei!=direction))\
-                and not((dir_nei in 'LR' and direction in 'lR' and dir_nei!=direction)):
-                    insort(
-                        to_visit,
-                        (
-                            nei,
-                            heatloss+map(*nei),
-                            nb_direction*(dir_nei==direction)+1,
-                            dir_nei,    
-                            path+dir_nei,
-                        ),
-                        key = lambda x: x[1],
-                    )
-        return map.map, best_path
+            best_path = None
+
+            while to_visit:
+                coords, heatloss, nb_direction, direction, path = to_visit.pop(0)
+                if path[-3:] in visited[coords] : continue
+                visited[coords].append(path[-3:])
+                print(coords, len(to_visit))
+                if coords == (map.height-1, map.width-1) : 
+                    best_path = heatloss
+                    print(path)
+                    break
+                for nei,dir_nei in [
+                    (map.up(    *coords),'U'), 
+                    (map.down(  *coords),'D'), 
+                    (map.right( *coords),'R'), 
+                    (map.left(  *coords),'L'),
+                ]:
+                    if nei and (dir_nei!=direction or nb_direction<3)\
+                    and dir_nei != REVERSED[direction]:
+                        insort(
+                            to_visit,
+                            (
+                                nei,
+                                heatloss+map(*nei),
+                                nb_direction*(dir_nei==direction)+1,
+                                dir_nei,    
+                                path+dir_nei,
+                            ),
+                            key = lambda x: x[1],
+                        )
+            return map.map, best_path
     
     def generate_view(self, structure: Any) -> str:
         result = '\n'
-        for line in structure:
-            for e in line : result += str(e)
-            result+='\n'
+        result += "\n".join(
+            "".join(str(e) for e in line)
+            for line in structure
+        )
         return result
