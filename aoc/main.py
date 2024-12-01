38b137bc6f56ff17a7ef321b8786e450209590e3
MustaphaEllouze <ellouze.mostafa@gmail.com>
2024-12-01 07:43:53 +0100

    arguments parsing and factorization

--- aoc/main.py
+++ aoc/main.py
@@ -13,23 +13,24 @@ def main(
         solver_file=importlib.import_module(name=path_to_solver)
         solver_class=solver_file.Solver
     except Exception as e:
-        print(f'No solver found: YEAR={year}; DAY={day}')
-        quit()
+        print(f'No valid solver found: YEAR={year}; DAY={day}')
+        raise e
     
+    input_name = 'input' if not other_file else other_file
+
     solver:ABCSolver=solver_class(
         folder=f'./aoc/Y{year}/D{day}',
-        input_file='.input' if not other_file else f'.{other_file}',
+        input_file=f'.{input_name}',
     )
 
     structure, solution=solver.export_result(part2=part2)
     
+    prefix =  f'for Y{year}|D{day}|{"part1" if not part2 else "part2"}'\
+              f'({input_name})'
+
     if view: 
         view_structure=solver.export_view(structure)
-        print(
-            f'Solution for Y{year}|D{day}({"input" if not other_file else other_file}): {view_structure}'
-        )
+        print(f'View {prefix}: {view_structure}')
 
-    print(
-        f'Solution for Y{year}|D{day}({"input" if not other_file else other_file}): {solution}'
-    )
+    print(f'Solution {prefix}: {solution}')
     
--- run.py
+++ run.py
@@ -1,9 +1,88 @@
 import sys
 sys.dont_write_bytecode=1
 
-from parameters import (
-    YEAR, DAY, FILE, VIEW, PART2
-)
-from aoc import main
+from aoc import main as run_solver
 
-main(year=YEAR, day=DAY, other_file=FILE, view=VIEW, part2=PART2)
+import argparse
+
+from datetime import datetime
+
+def read_args()->argparse.Namespace:
+
+    parser = argparse.ArgumentParser(
+        prog='python run.py',
+        description='Advent of Code - A set tools to ease Advent of Code solving, by Mustapha ELLOUZE'
+    )
+
+    parser.add_argument(
+        '-y', '--year', '-Y', '--YEAR',
+        help='Year of AOC to solve',
+        nargs=1,
+    )
+    
+    parser.add_argument(
+        '-d', '--day', '-D', '--DAY',
+        help='Day of AOC to solve',
+        nargs=1,
+    )
+    
+    parser.add_argument(
+        '-i', '--input', '-I', '--INPUT',
+        help='Input to solve',
+        nargs=1,
+    )
+
+    parser.add_argument(
+        '-v', '--view', '-V', '--VIEW',
+        help='Generate a view or not',
+        action='store_true'
+    )
+    
+    parser.add_argument(
+        '-p2', '--part2', '-P2', '--PART2',
+        help='Specify to solve part 2',
+        action='store_true'
+    )
+    
+    parser.add_argument(
+        '-b', '--both_parts', '-B', '--BOTH_PARTS',
+        help='Specify to solve both parts',
+        action='store_true'
+    )
+
+    return parser.parse_args()
+
+def main():
+
+    now = datetime.now()
+
+    script_arguments = read_args()
+
+    year = script_arguments.year[0] if script_arguments.year else now.year
+    day = script_arguments.day[0] if script_arguments.day else now.day
+    input = script_arguments.input[0] if script_arguments.input else 'input'
+    view = script_arguments.view
+    part2 = script_arguments.part2
+    both_parts = script_arguments.both_parts
+
+    # Run part 1
+    if both_parts or not part2 :
+        run_solver(
+            year=year,
+            day=day,
+            other_file=input,
+            view=view,
+            part2=False
+        )
+
+    # Run part 2
+    if both_parts or part2:
+        run_solver(
+            year=year,
+            day=day,
+            other_file=input,
+            view=view,
+            part2=True
+        )
+
+if __name__ == '__main__' : main()
