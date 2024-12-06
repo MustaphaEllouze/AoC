9a1b653995b892a340b7b09244d97b347b5098b3
MustaphaEllouze <ellouze.mostafa@gmail.com>
2024-12-06 07:56:55 +0100

    2024-6

--- aoc/Y2024/D6/solver.py
+++ aoc/Y2024/D6/solver.py
@@ -6,11 +6,11 @@ import os
 
 from aoc.tools.map import Map
 
-def does_it_finish(map_init:Map, pos_line_obs:int, pos_col_obs:int)->tuple[Map,int,bool]:
+def does_it_finish(map_init:Map, pos_line_obs:int, pos_col_obs:int, start_pos:tuple[int, int])->tuple[Map,int,bool]:
 
-    map = deepcopy(map_init)
+    map = map_init
 
-    pos_line, pos_col = map.find(value='^')
+    pos_line, pos_col = start_pos
     direction = 'U'
 
     if pos_line_obs and pos_col_obs : 
@@ -61,15 +61,16 @@ def does_it_finish(map_init:Map, pos_line_obs:int, pos_col_obs:int)->tuple[Map,i
                 else:
                     pos_line, pos_col = next_pos
     
-    return map.map, len(map.findall(value='X')), True
+    return map, len(map.findall(value='X')), True
 
-def check_dot(map:Map, dot:tuple[int, int]):
+def check_dot(map:Map, dot:tuple[int, int], start_pos:tuple[int, int]):
     copy_map = deepcopy(map)
     copy_map.map[*dot] = '#'
     return does_it_finish(
         map_init=copy_map, 
         pos_line_obs=dot[0],
         pos_col_obs=dot[1],
+        start_pos=start_pos,
         )[2]
 
 class Solver(ABCSolver):
@@ -77,27 +78,40 @@ class Solver(ABCSolver):
     def solve(self, part2:bool=False) ->tuple[Any, str]:
 
         map = Map([list(line) for line in self.data])
+        start_pos = map.find('^')
 
         if not part2 :
 
             return does_it_finish(
-                map_init = map,
+                map_init = deepcopy(map),
                 pos_line_obs=None,
                 pos_col_obs=None,
+                start_pos=start_pos,
             )[0:2]
 
         if part2 :
             
-            all_dots = map.findall(value='.')
-
+            # Only check the intial guard path
+            part_1 = does_it_finish(
+                map_init = deepcopy(map),
+                pos_line_obs=None,
+                pos_col_obs=None,
+                start_pos=start_pos,
+            )
+            all_dots = [
+                e 
+                for e in part_1[0].findall(value='X')
+                if e!= start_pos
+            ]
+            
             result = 0
 
             def process_dot(dot):
-                is_loop = not check_dot(map, dot)
+                is_loop = not check_dot(map, dot, start_pos)
                 if is_loop : print(f"Loop found: {dot}")
                 return is_loop
 
-            with ThreadPoolExecutor() as executor:
+            with ThreadPoolExecutor(max_workers=1) as executor:
                 futures = []
                 for dot in all_dots:
                     future = executor.submit(process_dot, dot)
