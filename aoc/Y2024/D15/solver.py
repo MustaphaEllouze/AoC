8375c27f45957d7de89bb10be72bf10043f71636
MustaphaEllouze <ellouze.mostafa@gmail.com>
2024-12-15 23:22:33 +0100

    2024-15 rewrite but does not work

--- aoc/Y2024/D15/solver.py
+++ aoc/Y2024/D15/solver.py
@@ -1,5 +1,6 @@
 from typing import Any
 from aoc.tools import ABCSolver
+from dataclasses import dataclass
 
 from aoc.tools import Map
 
@@ -9,6 +10,215 @@ DIRECTIONS = {
             '>' : 'R',
             '<' : 'L',
         }
+
+@dataclass
+class Block:
+    movable:bool
+    position:tuple[int, int]
+    position2:tuple[int, int]
+    symbol:str
+    want_push:bool
+    propagates:bool
+
+class Problem:
+
+    def __init__(
+            self,
+            map:Map,
+            commands:list[str],
+            part2:bool,
+        ) -> None:
+        self.blocks:dict[tuple[int, int]:Block] = {}
+        for position in map.iterator:
+            symbol = map(*position)
+            if symbol == ']' : 
+                self.blocks[position] = self.blocks[
+                    (position[0], position[1]-1)
+                ]
+            else:
+                self.blocks[position] = Block(
+                    movable= (symbol!='#'),
+                    position=position,
+                    position2=(position[0], position[1]+1) if symbol=='[' else None,
+                    symbol=symbol,
+                    want_push=False,
+                    propagates=not (symbol in ['#', '.']),
+                )
+        self.commands = commands
+        self.map = map
+        self.part2 = part2
+        self.cur_pos = self.map.find(value='@')
+    
+    def clean_state(self):
+        for bb in self.blocks.values():
+            bb.want_push = False
+    
+    def propagate_push(
+            self, 
+            position:tuple[int, int],
+            direction:str,
+        )->list[tuple[int, int]]:
+            cur_block:Block = self.blocks[position]
+            if not cur_block.propagates : return []
+            match direction:
+                case 'L':
+                    propagate_those = [self.map.left(*position)]
+                case 'R':
+                    if cur_block.symbol=='[':
+                        propagate_those = [self.map.right(*self.map.right(*position))]
+                    else:
+                        propagate_those = [self.map.right(*position)]
+                case 'D':
+                    if cur_block == '[':
+                        propagate_those = [
+                            self.map.down(*position),
+                            self.map.right(*self.map.down(*position)),
+                        ]
+                    else:
+                        propagate_those = [self.map.down(*position)]
+                case 'U':
+                    if cur_block == '[':
+                        propagate_those = [
+                            self.map.up(*position),
+                            self.map.right(*self.map.up(*position)),
+                        ]
+                    else:
+                        propagate_those = [self.map.up(*position)]
+            for b in propagate_those :
+                self.blocks[*b].want_push = True
+            return propagate_those
+
+    
+    def advance_one_step(self, direction:str):
+        self.clean_state()
+        self.blocks[self.cur_pos].want_push = True
+        to_visit = [self.cur_pos]
+        visited = []
+        while to_visit:
+            cur_block = to_visit.pop()
+            if cur_block in visited : continue
+            visited.append(cur_block)
+            to_visit += self.propagate_push(
+                position=cur_block,
+                direction=direction,
+            )
+        all_blocks_wanting:list[tuple[int, int]] = [
+            block
+            for block in visited
+            if self.blocks[block].want_push
+        ]
+        # print([b.position for b in all_blocks_wanting])
+        # print([b.movable for b in all_blocks_wanting])
+        if any([not self.blocks[b].movable for b in all_blocks_wanting]):
+            return
+        else:
+            # print('GOT HERE')
+            # print('cur_pos', self.cur_pos)
+            for da_block_pos,b in zip(
+                    all_blocks_wanting,
+                    [self.blocks[e] for e in all_blocks_wanting],
+            ):
+                if b.symbol == '.' : continue
+                if b.symbol != '[':
+                    match direction :
+                        case 'U' :
+                            b.position = (b.position[0]-1, b.position[1])
+                        case 'D' :
+                            b.position = (b.position[0]+1, b.position[1])
+                        case 'L' :
+                            b.position = (b.position[0], b.position[1]-1)
+                        case 'R' :
+                            b.position = (b.position[0], b.position[1]+1)
+                    self.blocks[b.position] = b 
+                # elif da_block_pos != b.position: 
+                #     print('SORRY BRO', da_block_pos, b.position)
+                #     continue
+                else: 
+                    # print(self.blocks[b.position2])
+                    # print('Wesh', b)
+                    if self.blocks[b.position2] == b :
+                        print('pop pos2', b.position2)
+                        if self.blocks[b.position2]:
+                            self.blocks.pop(b.position2)
+                            # self.blocks.pop(b.position)
+                    #     self.blocks[b.position2] = Block(
+                    #         movable=True, position=b.position2, 
+                    #         position2=None, symbol='.', want_push=False,
+                    #         propagates=False,
+                    #     )
+                    match direction :
+                        case 'U' :
+                            b.position = (b.position[0]-1, b.position[1])
+                        case 'D' :
+                            b.position = (b.position[0]+1, b.position[1])
+                        case 'L' :
+                            b.position = (b.position[0], b.position[1]-1)
+                        case 'R' :
+                            b.position = (b.position[0], b.position[1]+1)
+                    b.position2 = (b.position[0], b.position[1]+1)
+                    self.blocks[b.position] = b
+                    self.blocks[b.position2] = b
+            self.blocks[self.cur_pos] = Block(
+                movable=True, position=self.cur_pos, 
+                position2=None, symbol='.', want_push=False,
+                propagates=False,
+            )
+            match direction:
+                case 'U':
+                    self.cur_pos = (self.cur_pos[0]-1, self.cur_pos[1])
+                case 'D':
+                    self.cur_pos = (self.cur_pos[0]+1, self.cur_pos[1])
+                case 'L':
+                    self.cur_pos = (self.cur_pos[0], self.cur_pos[1]-1)
+                case 'R':
+                    self.cur_pos = (self.cur_pos[0], self.cur_pos[1]+1)
+            
+            # print('cur_pos', self.cur_pos)
+    def populate_missing(self):
+        for pos in self.map.iterator:
+            if not pos in self.blocks.keys():
+                if (pos[0], pos[1]-1) in self.blocks.keys():
+                    if self.blocks[(pos[0], pos[1]-1)].symbol == '[':
+                        self.blocks[pos] = self.blocks[(pos[0], pos[1]-1)]
+                    else:   
+                        self.blocks[pos] = Block(
+                            movable=True,
+                            position=pos,
+                            position2=None,
+                            symbol='.',
+                            want_push=False,
+                            propagates=False,
+                        )
+
+    def update_map(self):
+        for pos,b in self.blocks.items():
+            self.map.map[*(b.position)] = b.symbol
+            if b.position2 :
+                assert b.symbol == '['
+                self.map.map[*(b.position2)] = ']'
+
+    def solve(self):
+        for i,c in enumerate(self.commands):
+            print(f'Step {i} / {len(self.commands)}', end='\r')
+            print('direction', c)
+            self.advance_one_step(
+                direction=DIRECTIONS[c],
+            )
+            self.populate_missing()
+            self.update_map()
+            print(self.map)
+            print(self.blocks[(5, 11)])
+            print(self.blocks[(6, 11)])
+            input()
+    
+    def get_result(self):
+        result = 0
+        for pos,b in self.blocks.items():
+            if b.symbol == 'O' : 
+                result += pos[0]*100 + pos[1]
+        return result
+
+
 
 def can_push(
         current_position:tuple[int, int],
@@ -209,28 +419,39 @@ class Solver(ABCSolver):
 
         map = Map(raw_data=split_lines)
         commands = list(''.join(split_lines2))
-
-        cur_pos = map.find(value='@')
         print(map)
-        for c in commands: 
-            print('direction', c)
-            cur_pos = push(
-                current_position=cur_pos,
-                command=c,
-                map=map,
-                part2=part2,
-            )
-            print(map)
-            input()
-        if not part2:
-            for o in map.findall('O'):
-                result += o[0]*100+o[1]
-        else:
-            for o in map.findall('['):
-                result += min(o[0], map.height-o[0])*100
-                result += min(o[1], map.width-o[1]-1)
 
-        return map, result
+        problem = Problem(
+            map=map, commands=commands, part2=part2,
+        )
+
+        # print(problem.map)
+        # for b,bb in problem.blocks.items():
+        #     print(b, bb)
+        
+        problem.solve()
+
+        # cur_pos = map.find(value='@')
+        # print(map)
+        # for c in commands: 
+        #     print('direction', c)
+        #     cur_pos = push(
+        #         current_position=cur_pos,
+        #         command=c,
+        #         map=map,
+        #         part2=part2,
+        #     )
+        #     print(map)
+        #     input()
+        # if not part2:
+        #     for o in map.findall('O'):
+        #         result += o[0]*100+o[1]
+        # else:
+        #     for o in map.findall('['):
+        #         result += min(o[0], map.height-o[0])*100
+        #         result += min(o[1], map.width-o[1]-1)
+
+        return problem.map, problem.get_result()
                     
     def generate_view(self, structure: Any) -> str:
         return super().generate_view(structure)
