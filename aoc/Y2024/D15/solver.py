1373617e04054b9d59764d3ac1d6f22bddc2ceb0
MustaphaEllouze <ellouze.mostafa@gmail.com>
2024-12-16 00:06:27 +0100

    2024-15 cleaning done

--- aoc/Y2024/D15/solver.py
+++ aoc/Y2024/D15/solver.py
@@ -111,21 +111,12 @@ class Problem:
             for block in visited
             if self.blocks[block].want_push
         ]
-        # print([b.position for b in all_blocks_wanting])
-        # print([b.movable for b in all_blocks_wanting])
         if any([not self.blocks[b].movable for b in all_blocks_wanting]):
             return
         else:
-            # print('GOT HERE')
-            # print('cur_pos', self.cur_pos)
-            for da_block_pos,b in zip(
-                    all_blocks_wanting,
-                    [self.blocks[e] for e in all_blocks_wanting],
-            ):
+            for b in [self.blocks[e] for e in all_blocks_wanting]:
                 if b.symbol == '.' : continue
-                # print('CURRENTLY DISPLACING', b)
                 if self.blocks[b.position] is b :
-                    # print('POPED')
                     self.blocks.pop(b.position)
                 match direction :
                     case 'U' :
@@ -152,7 +143,6 @@ class Problem:
                 case 'R':
                     self.cur_pos = (self.cur_pos[0], self.cur_pos[1]+1)
             
-            # print('cur_pos', self.cur_pos)
     def populate_missing(self):
         for pos in self.map.iterator:
             if not pos in self.blocks.keys():
@@ -171,14 +161,11 @@ class Problem:
     def solve(self):
         for i,c in enumerate(self.commands):
             print(f'Step {i} / {len(self.commands)}', end='\r')
-            # print('direction', c)
             self.advance_one_step(
                 direction=DIRECTIONS[c],
             )
             self.populate_missing()
             self.update_map()
-            # print(self.map)
-            # input()
     
     def get_result(self):
         result = 0
@@ -189,179 +176,6 @@ class Problem:
                 result += pos[0]*100
                 result += pos[1]
         return result
-
-
-
-def can_push(
-        current_position:tuple[int, int],
-        command:str,
-        map:Map,
-)->tuple[bool, tuple[int, int], tuple[int, int]]:
-    closest_dot = map.find_in_direction(
-        *current_position,
-        direction=DIRECTIONS[command],
-        value='.'
-    )
-    closest_wall = map.find_in_direction(
-        *current_position,
-        direction=DIRECTIONS[command],
-        value='#'
-    )
-    if not closest_dot : 
-        return False, None
-    if abs(current_position[0]-closest_wall[0])+abs(current_position[1]-closest_wall[1]) \
-        < abs(current_position[0]-closest_dot[0])+abs(current_position[1]-closest_dot[1]):
-        return False, None
-    return True, closest_dot
-
-def push(
-        current_position:tuple[int, int],
-        command:str,
-        map:Map,
-        part2:bool,
-        move_bonhomme:bool=True,
-)->tuple[int, int]:
-    print('push', current_position, command, part2, move_bonhomme)
-    can_be_pushed, closest_dot = can_push(
-        current_position=current_position,
-        command=command,
-        map=map,
-    )
-    if not can_be_pushed : return current_position
-    if not part2:
-        to_move = map.list_between_two(
-                        *current_position,
-                        *closest_dot,
-                    )
-        if DIRECTIONS[command] in ['U', 'L'] : 
-            to_move = to_move[::-1]
-        if move_bonhomme:
-            symbols = ['.']+[map(*o) for o in to_move][:-1]
-            print('MOVING THESE GUYS', to_move)
-            for o,no in zip(to_move, symbols): 
-                map.map[o] = no
-        match DIRECTIONS[command]:
-            case 'U': current_position = (current_position[0]-1, current_position[1])
-            case 'D': current_position = (current_position[0]+1, current_position[1])
-            case 'L': current_position = (current_position[0], current_position[1]-1)
-            case 'R': current_position = (current_position[0], current_position[1]+1)
-        return current_position
-    else:
-        if DIRECTIONS[command] in ['L', 'R']:
-            return push(
-                current_position=current_position, 
-                command=command, 
-                map=map, 
-                part2=not part2,
-            )
-        else :
-            dry_run_pos = push2(
-                command=command,
-                current_position=current_position,
-                closest_dot=closest_dot,
-                map=map,
-                move_bonhomme=False
-            )
-            if dry_run_pos != current_position:
-                return push2(
-                    command=command,
-                    current_position=current_position,
-                    closest_dot=closest_dot,
-                    map=map,
-                    move_bonhomme=True,
-                )
-    
-def push2(
-        command:str,
-        current_position:tuple[int, int],
-        closest_dot:tuple[int, int],
-        map:Map,
-        move_bonhomme:bool,
-)->tuple[int, int]:
-    print('push2', command, current_position, closest_dot, 'DRY_RUN' if not move_bonhomme else 'WET_RUN')
-    to_move = map.list_between_two(
-                    *current_position,
-                    *closest_dot,
-                )
-    if DIRECTIONS[command] == 'U' :
-        to_move = to_move[::-1]
-    symbols = [map(*o) for o in to_move]
-    print('TRY TO MOVE THIS', to_move, symbols)
-    if not '[' in symbols and not ']' in symbols:
-        return push(
-            current_position=current_position,
-            command=command,
-            map=map,
-            part2=False,
-            move_bonhomme=move_bonhomme,
-        )
-    else:
-        is_down_factor = 1 if DIRECTIONS[command] == 'D' else -1
-        central_future_pos = push(
-                current_position=current_position,
-                command=command,
-                map=map,
-                move_bonhomme=move_bonhomme,
-                part2=False,
-            )
-        if central_future_pos == current_position : 
-            print('CENTRAL WAS NOT MOVABLE')
-            return current_position
-        print('CENTER CAN BE PUSHED')
-        print('ITERATING ON SYMBOLS', to_move)
-        for i,pos_sym in enumerate(to_move):
-            sym = map(*pos_sym)
-            if sym in ['@', '.'] : continue
-            elif sym == '[' :
-                print('COMPUTE [')
-                can_be_pushed, c_dot = can_push(
-                    current_position=(
-                        current_position[0]+is_down_factor*i,
-                        current_position[1]+1
-                    ),
-                    command=command,
-                    map=map,
-                )
-                if not can_be_pushed : return current_position
-                print('CAN BE PUSHED ALONE? checking other part!')
-                alrededor_future_pose = push2(
-                    current_position=(
-                        current_position[0]+is_down_factor*i,
-                        current_position[1]+1
-                    ),
-                    command=command,
-                    map=map,
-                    move_bonhomme=move_bonhomme,
-                    closest_dot=c_dot
-                )
-            elif sym == ']' :
-                print('Compute ]')
-                can_be_pushed, c_dot = can_push(
-                    current_position=(
-                        current_position[0]+is_down_factor*i,
-                        current_position[1]-1
-                    ),
-                    command=command,
-                    map=map,
-                )
-                if not can_be_pushed : return current_position
-                print('CAN BE PUSHED ALONE? checking other part!!')
-                alrededor_future_pose = push2(
-                    current_position=(
-                        current_position[0]+is_down_factor*i,
-                        current_position[1]-1
-                    ),
-                    command=command,
-                    map=map,
-                    move_bonhomme=move_bonhomme,
-                    closest_dot=c_dot,
-                )
-            else : raise NotImplementedError(f'Wrong symbol : {sym}')
-            if alrededor_future_pose == current_position:
-                print('this symbol is unmovable')
-                return current_position
-        print('EVERYTHING COULD BE PUSHED !!!!! FROM HERE', current_position)
-        return central_future_pos
 
 class Solver(ABCSolver):
 
@@ -396,32 +210,8 @@ class Solver(ABCSolver):
         problem = Problem(
             map=map, commands=commands, part2=part2,
         )
-
-        # print(problem.map)
-        # for b,bb in problem.blocks.items():
-        #     print(b, bb)
         
         problem.solve()
-
-        # cur_pos = map.find(value='@')
-        # print(map)
-        # for c in commands: 
-        #     print('direction', c)
-        #     cur_pos = push(
-        #         current_position=cur_pos,
-        #         command=c,
-        #         map=map,
-        #         part2=part2,
-        #     )
-        #     print(map)
-        #     input()
-        # if not part2:
-        #     for o in map.findall('O'):
-        #         result += o[0]*100+o[1]
-        # else:
-        #     for o in map.findall('['):
-        #         result += min(o[0], map.height-o[0])*100
-        #         result += min(o[1], map.width-o[1]-1)
 
         return problem.map, problem.get_result()
                     
