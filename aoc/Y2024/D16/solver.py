de555ffd9d615ac08fcc16af3a3126628278d05d
MustaphaEllouze <ellouze.mostafa@gmail.com>
2024-12-16 11:19:37 +0100

    2024-16 part2 not working

--- aoc/Y2024/D16/solver.py
+++ aoc/Y2024/D16/solver.py
@@ -30,14 +30,14 @@ class Solver(ABCSolver):
         visited = []
 
         while to_visit and pos != goal:
-            print(len(to_visit), abs(pos[0]-goal[0])+abs(pos[1]-goal[1]))
+            # print(len(to_visit), abs(pos[0]-goal[0])+abs(pos[1]-goal[1]))
             pos, direc, score, dico = to_visit.pop(0)
             if (pos,direc) in visited : 
                 continue
             visited.append((pos, direc))
 
-            insort(to_visit, (pos, clockwise[direc], score+1000, dico), key=lambda x:x[2])
-            insort(to_visit, (pos, trigowise[direc], score+1000, dico), key=lambda x:x[2])
+            insort(to_visit, (pos, clockwise[direc], score+1000, {**dico, **{pos:score+1000}}), key=lambda x:x[2])
+            insort(to_visit, (pos, trigowise[direc], score+1000, {**dico, **{pos:score+1000}}), key=lambda x:x[2])
 
             match direc:
                 case 'U':
@@ -58,7 +58,46 @@ class Solver(ABCSolver):
 
         if not part2 : return map, best_score
 
-        return map, 0
+        pos = map.find(value='S')
+        goal = map.find(value='E')
+
+        to_visit = [(pos, 'R', 0, {pos:0})]
+        visited = []
+
+        while to_visit and pos != goal:
+            # print(len(to_visit), abs(pos[0]-goal[0])+abs(pos[1]-goal[1]))
+            pos, direc, score, dico = to_visit.pop(0)
+            if pos in best_dico :
+                if best_dico[pos] == dico[pos] :
+                    print('upgrade', pos)
+                    best_dico = {**best_dico, **dico}
+            if (pos,direc) in visited : 
+                continue
+            visited.append((pos, direc))
+
+            insort(to_visit, (pos, clockwise[direc], score+1000, {**dico, **{pos:score+1000}}), key=lambda x:x[2])
+            insort(to_visit, (pos, trigowise[direc], score+1000, {**dico, **{pos:score+1000}}), key=lambda x:x[2])
+
+            match direc:
+                case 'U':
+                    if map(*map.up(*pos)) in ['.', 'E']:
+                        insort(to_visit, (map.up(*pos), direc, score+1, {**dico, **{map.up(*pos):score+1}}), key=lambda x:x[2])
+                case 'D':
+                    if map(*map.down(*pos)) in ['.', 'E']:
+                        insort(to_visit, (map.down(*pos), direc, score+1, {**dico, **{map.down(*pos):score+1}}), key=lambda x:x[2])
+                case 'R':
+                    if map(*map.right(*pos)) in ['.', 'E']:
+                        insort(to_visit, (map.right(*pos), direc, score+1, {**dico, **{map.right(*pos):score+1}}), key=lambda x:x[2])
+                case 'L':
+                    if map(*map.left(*pos)) in ['.', 'E']:
+                        insort(to_visit, (map.left(*pos), direc, score+1, {**dico, **{map.left(*pos):score+1}}), key=lambda x:x[2])
+
+        for pos in best_dico:
+            map.map[*pos] = 'O'
+        
+        # print(sorted(best_dico.items(), key=lambda x:x[1]))
+
+        return map, len(best_dico)
                     
     def generate_view(self, structure: Any) -> str:
         return super().generate_view(structure)
