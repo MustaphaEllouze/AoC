9d0a90beb4d9e91b3b2ce625415eab64af10187c
MustaphaEllouze <ellouze.mostafa@gmail.com>
2024-12-21 11:15:39 +0100

    2024-20 part2

--- aoc/Y2024/D20/solver.py
+++ aoc/Y2024/D20/solver.py
@@ -4,7 +4,7 @@ from aoc.tools import ABCSolver
 from aoc.tools import Map
 from collections import defaultdict
 
-def find_path(map:Map)->tuple[int, tuple[int, int]]:
+def find_path(map:Map)->tuple[int, int]:
     start_pos = map.find('S')
     end_pos = map.find('E')
     to_visit = [(start_pos, 0, (start_pos,))]
@@ -14,7 +14,7 @@ def find_path(map:Map)->tuple[int, tuple[int, int]]:
         if pos in visited : continue
         visited.append(pos)
         if pos == end_pos :
-            return (time,path)
+            return path
         for neigh in map.cardinal_neighbours(*pos):
             if map(*neigh) in ['.', 'E']:
                 to_visit.append((neigh, time+1, (*path, neigh)))
@@ -23,30 +23,25 @@ class Solver(ABCSolver):
 
     def solve(self, part2:bool=False) ->tuple[Any, str]:
 
-        result = 0
-
         map = Map(raw_data=self.data)
 
-        no_cheat_time,best_path = find_path(map)
+        best_path = find_path(map)
         found = defaultdict(int)
 
+        indexes = {t:i for i,t in enumerate(best_path)}
+
+        proximo = defaultdict(list)
         for i,pos in enumerate(best_path):
             print(f'{i}/{len(best_path)}', end='\r')
-            idx_pos = best_path.index(pos)
-            for pos_pos, mid_pos in (
-                (map.left(*map.left(*pos)), map.left(*pos)),
-                (map.up(*map.up(*pos)), map.up(*pos)),
-                (map.right(*map.right(*pos)), map.right(*pos)),
-                (map.down(*map.down(*pos)), map.down(*pos)),
-            ):
-                if pos_pos is None : continue
-                if not pos_pos in best_path : continue
-                if not map(*mid_pos) == '#' : continue
-                idx_pospos = best_path.index(pos_pos)
-                if idx_pospos < idx_pos : continue
-                found[idx_pospos-idx_pos-2] += 1
+            for posi in best_path:
+                if 0<abs(pos[0]-posi[0])+abs(pos[1]-posi[1])<=2+18*part2:
+                    proximo[pos].append(posi)
         
-        print(found)
+        for pos,prox in proximo.items():
+            for propro in prox:
+                time_gained =indexes[propro]-indexes[pos]-abs(pos[0]-propro[0])-abs(pos[1]-propro[1])
+                if time_gained>0:
+                    found[time_gained] += 1
 
         return None, sum([v for k,v in found.items() if k>=100])
                     
